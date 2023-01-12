1eebe53ba7d67221d8c9e15c507be9d11d785fc2
jsapro <77.3.77@mail.ru>
2023-01-12 15:55:38 +0300

    ready after fix

--- scripts/index.js
+++ scripts/index.js
@@ -8,34 +8,37 @@ let popupSubmitElement = document.querySelector('.popup__submit');
 let gridCardLikeElement = document.querySelector('.grid-card__like');
 let popupElement = document.querySelector('.popup');
 let popupInfoElement = document.querySelector('.popup__info');
-let popupNameElement = document.querySelector('.popup__name');
-let popupJobElement = document.querySelector('.popup__job');
+let popupNameElement = document.querySelector('.popup__input_type_name');
+let popupJobElement = document.querySelector('.popup__input_type_job');
 let profileNameElement = document.querySelector('.profile__name');
 let profileJobElement = document.querySelector('.profile__job');
 
 
-function popupOpend () {
+function openPopup () {
   popupElement.classList.add('popup_opened');
+  popupNameElement.value = profileNameElement.textContent;
+  popupJobElement.value = profileJobElement.textContent;
 }
 
-profileEditBtnElement.addEventListener('click', popupOpend);
+profileEditBtnElement.addEventListener('click', openPopup);
 
-function popupClosed () {
+function closePopup () {
   popupElement.classList.remove('popup_opened');
 }
 
-popupCloseBtnElement.addEventListener('click', popupClosed);
+popupCloseBtnElement.addEventListener('click', closePopup);
 
 function handleFormSubmit (e) {
   e.preventDefault();
+  profileNameElement.textContent = popupNameElement.value;
+  profileJobElement.textContent = popupJobElement.value;
+  closePopup();
 }
 
 popupInfoElement.addEventListener('submit', handleFormSubmit);
-popupInfoElement.addEventListener('submit', addInfo);
 
-function addInfo () {
-  profileNameElement.textContent = popupNameElement.value;
-  profileJobElement.textContent = popupJobElement.value;
-}
 
-popupSubmitElement.addEventListener('click', popupClosed);
+
+
+
+111
