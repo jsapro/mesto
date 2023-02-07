5a45410862991c616c2ec97e541063ddc713fca4
jsapro <77.3.77@mail.ru>
2023-02-07 12:24:13 +0300

    fix/improve popup width on medium screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
 let profileEditBtnElement = document.querySelector('.profile__edit-button');
 let profileAddBtnElement = document.querySelector('.profile__add-button');
 
@@ -6,7 +34,7 @@ let popupOpenedElement = document.querySelector('.popup_opened');
 let popupCloseBtnElement = document.querySelector('.popup__close-btn');
 let popupSubmitElement = document.querySelector('.popup__submit');
 let gridCardLikeElement = document.querySelector('.grid-card__like');
-let popupElement = document.querySelector('.popup');
+let popupElement = document.querySelector('.popup_edit-profile');
 let popupInfoElement = document.querySelector('.popup__info');
 let popupNameElement = document.querySelector('.popup__input_type_name');
 let popupJobElement = document.querySelector('.popup__input_type_job');
@@ -36,7 +64,4 @@ function handleFormSubmit (e) {
 }
 
 popupInfoElement.addEventListener('submit', handleFormSubmit);
-
-//test pull request-3
-
 
